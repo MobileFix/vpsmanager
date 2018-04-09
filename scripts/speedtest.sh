93419004876378638efb14e6e099cc10e1b9bbae
twossh <38133710+twossh@users.noreply.github.com>
2018-04-09 03:01:56 -0300

    Update speedtest.sh

--- scripts/speedtest.sh
+++ scripts/speedtest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
+clear
 tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-15s\n' "SpeedTest" ; tput sgr0
 echo ""
 sleep 2
-clear
 echo ""
 tput setaf 7 ; tput setab 1 ; printf '  %-30s%s\n' "------------------------------------------------------------------" ; echo "" ; tput sgr0
 cd /bin*
