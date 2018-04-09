51f2d35b239842d5e5b5c17924cf61e02a4cd35d
twossh <38133710+twossh@users.noreply.github.com>
2018-04-09 02:41:37 -0300

    Update vpsmanagersetup.sh

--- vpsmanagersetup.sh
+++ vpsmanagersetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-20s\n' "TwoSSH " ; tput sgr0
 tput setaf 3 ; tput bold ; echo "" ; echo "Este script irá:" ; echo ""
 echo "● Instalar e configurar o proxy squid nas portas 80, 3128, 8080 e 8799" ; echo "  para permitir conexões SSH para este servidor"
@@ -184,6 +185,7 @@ then
 		/etc/init.d/ssh reload > /dev/null
 	fi
 fi
+clear
 echo ""
 tput setaf 7 ; tput setab 4 ; tput bold ; echo "Proxy Squid Instalado e rodando nas portas: 80, 3128, 8080 e 8799" ; tput sgr0
 tput setaf 7 ; tput setab 4 ; tput bold ; echo "OpenSSH rodando nas portas 22 e 443" ; tput sgr0
