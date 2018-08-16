8bfd06ab0738b1634c292e58b472dda601b61044
twossh <38133710+twossh@users.noreply.github.com>
2018-08-16 10:08:58 -0300

    Update badauto.sh

--- scripts/badauto.sh
+++ scripts/badauto.sh
@@ -1,6 +1,4 @@
-NUMBER=$(ps -x | grep -c "badvpn-udpgw")
-if [ $NUMBER = "3" ]; then
-    echo "" > /dev/null
-else
-    screen -dmS badvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
-fi
+#!/bin/bash
+sleep 30
+screen -dmS badvpn /bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients$
+exit
