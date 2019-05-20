56028fde1cb1bf9dbd99edc87528aedd3191029f
twossh <38133710+twossh@users.noreply.github.com>
2019-05-20 00:44:36 -0300

    Create update.sh

---
+++ scripts/update.sh
@@ -0,0 +1,13 @@
+apt-get update
+apt-get upgrade -y
+apt-get autoremove -y
+apt-get dist-upgrade -y
+apt-get install sudo -y
+sudo rm -rf /var/lib/dpkg/info/*.*
+sudo apt-get clean
+sudo apt-get autoclean
+sudo apt-get install -f
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get dist-upgrade -y
